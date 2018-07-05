a639e5edb5ae43043ca30d79139a3636e9415193
ipeinado <i.peinado.martinez@gmail.com>
2018-07-05 13:13:12 +0200

    added bootstrap and morphic themes

---
+++ themes/custom/morphic/gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	sourcemaps = require('gulp-sourcemaps'),
+	browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync', ['sass'], function() {
+	browserSync.init({
+		proxy:  "http://morphic8.lndo.site",
+		browserSyncPort: 3003,
+		reloadDelay: 1,
+		open: false
+	});
+});
+
+gulp.task('sass', function() {
+	return gulp.src('./scss/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(autoprefixer(['last 2 versions', '> 1%', 'ie 8']))
+		.pipe(sourcemaps.write('./css'))
+		.pipe(gulp.dest('./css'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', ['browser-sync'], function() {
+	return gulp
+		.watch(['./scss/**/*.scss'], ['sass']);
+});
+
+gulp.task('default', ['watch', 'browser-sync']);
