89b0aa6b0e9fbd7dd3dcc52267a379b226f9cb72
ipeinado <i.peinado.martinez@gmail.com>
2018-11-08 11:56:25 +0100

    made homepage link blocks clickable (with js) and styled hero

--- themes/custom/morphic/js/homepage.js
+++ themes/custom/morphic/js/homepage.js
@@ -7,6 +7,11 @@
 				var h3 = $(this).find('h3').text().trim();
 				var link = $(this).find('a');
 				$(link).attr('title', 'Visit the "' + h3 + '" page');
+
+				var url = $(link).attr('href');
+				$(this).click(function(e) {
+					window.location.href = url;
+				});
 			});
 		}
 	};
